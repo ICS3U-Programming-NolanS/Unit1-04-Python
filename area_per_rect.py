6d179a7d0689876c8ac06fbd59a5aa6fedfc278d
Nolan Shami <nolan.shami@stu.ocsb.ca>
2022-09-27 18:43:37 +0000

    Added area_per_rect.py

---
+++ area_per_rect.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+# Created by: Nolan
+# Created on: September 27, 2022
+# This program calculates and displays the area and perimeter of a 
+# rectangle with length 6cm and width 2cm.
+
+def main():
+    print ("For a rectangle that has dimensions")
+    print ("length 6cm and width 2cm:")
+    print ("")
+    print ("Area = {} cm^2". format (6*2))
+    print ("Perimeter = {} cm". format (2*(6+2)))
+
+
+if __name__ == "__main__":
+  main()
---
+++ basic_math.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# Created By: Nolan
+# Date: Sep. 20, 2022
+# This program does the basics of mathematics.
+ 
+def main():
+    print("6-4={}". format(6-4))
+    print("7+3={}". format(7+3))
+    print("6*3={}". format(6*3))
+    print("9/9={}". format(9/9))
+    print("5+4*3={}". format(5+4*3))
+    print("4^3={}". format(4^3))
+    print("4**3={}". format(4**3))
+    print("16**0.5={}". format(16**0.5))
+ 
+ 
+if __name__ == "__main__":
+    main()
